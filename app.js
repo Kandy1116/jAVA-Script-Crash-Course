19a8d0c1e7aca88e2b0abbdf56a549ec4b5a5332
Kandy1116 <venus.kandy@gmail.com>
2025-10-16 14:43:52 -0500

    update work

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-function toggleDarkMode() {
-   document.querySelector('body').classList.toggle("dark-theme")
-}
+// (fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+// 1. Then
+(fetch("https://jsonplaceholder.typicode.com/users/1")).then(response => {
+   response.json().then(data => {
+      console.log(data)
+   })
+})
